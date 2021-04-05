dfaac6f2e94b06adc9beb42da6c8b62b118adcd8
KimJH <98kimjh@gmail.com>
2021-04-05 20:06:32 +0900

    Attach SignUpPage (시작 페이지 바꿔 두었음, 저장하기 눌리면 MainActivity로 이동 할꺼)

--- app/src/main/java/com/example/myapplication/SignUpActivity.java
+++ app/src/main/java/com/example/myapplication/SignUpActivity.java
@@ -2,13 +2,34 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private Button btn_input_save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        btn_input_save = findViewById(R.id.btn_input_save);
+
+        btn_input_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 넘겨줄 값들 나중에 다시 적어야함 (e.g. 개인정보, 분야 등 입력받은 값)
+//                str = editText.getText().toString();
+                Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+//                intent.putExtra("str", str);
+                startActivity(intent);  //  activity 이동
+
+
+                finish();
+            }
+        });
     }
 }
