34d079a325a90f785b1efd6f42a7341dcb8db870
KimJH <98kimjh@gmail.com>
2021-04-06 14:05:27 +0900

    Change Login & Register

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -2,10 +2,12 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -13,6 +15,7 @@ public class LoginActivity extends AppCompatActivity {
     private Button btn_google;
     private Button btn_fb;
     private Button btn_twt;
+    private Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +26,23 @@ public class LoginActivity extends AppCompatActivity {
         btn_google = findViewById(R.id.btn_google);
         btn_fb = findViewById(R.id.btn_facebook);
         btn_twt = findViewById(R.id.btn_twitter);
+        btn_login = findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 로그인 세션 처리 후 메인화면으로 이동
+                /*
+                로그인 세션 처리 및 정보 전달 code 구간
+                 */
+                // if (로그인 성공 시) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);  //  activity 이동
+
+                finish();
+                // }
+            }
+        });
 
         clickBtn(btn_signup);
         clickSNS(btn_google);
@@ -34,12 +54,10 @@ public class LoginActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 나중에 소셜 로그인은 따로 만들어 주어야함 (각각 API를 통해서)
-                Intent intent = new Intent(LoginActivity.this, UserDetailsActivity.class);
-                startActivity(intent);  //  activity 이동
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivityForResult(intent, 0);  //  activity 이동
 
-                // 회원가입 후 다시 로그인 창으로 돌아오기 위해 finish 보류
-                //finish();
+//                finish();
             }
         });
     }
@@ -53,9 +71,38 @@ public class LoginActivity extends AppCompatActivity {
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(intent);  //  activity 이동
 
-                finish();
+                // 회원가입 후 다시 로그인 창으로 돌아오기 위해 finish 보류
+                //finish();
             }
         });
     }
 
+    // 마지막으로 뒤로 가기 버튼을 눌렀던 시간 저장
+    private long backKeyPressedTime = 0;
+    // 첫 번째 뒤로 가기 버튼을 누를 때 표시
+    private Toast toast;
+
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+        // 기존 뒤로 가기 버튼의 기능을 막기 위해 주석 처리 또는 삭제
+
+        // 마지막으로 뒤로 가기 버튼을 눌렀던 시간에 2초를 더해 현재 시간과 비교 후
+        // 마지막으로 뒤로 가기 버튼을 눌렀던 시간이 2초가 지났으면 Toast 출력
+        // 2000 milliseconds = 2 seconds
+        if (System.currentTimeMillis() > backKeyPressedTime + 2000) {
+            backKeyPressedTime = System.currentTimeMillis();
+            toast = Toast.makeText(this, "뒤로 가기 버튼을 한 번 더 누르시면 종료됩니다.", Toast.LENGTH_LONG);
+            toast.show();
+            return;
+        }
+        // 마지막으로 뒤로 가기 버튼을 눌렀던 시간에 2초를 더해 현재 시간과 비교 후
+        // 마지막으로 뒤로 가기 버튼을 눌렀던 시간이 2초가 지나지 않았으면 종료
+        if (System.currentTimeMillis() <= backKeyPressedTime + 2000) {
+            finish();
+            toast.cancel();
+            toast = Toast.makeText(this, "이용해 주셔서 감사합니다.", Toast.LENGTH_LONG);
+            toast.show();
+        }
+    }
 }
--- app/src/main/java/com/example/myapplication/RegisterActivity.java
+++ app/src/main/java/com/example/myapplication/RegisterActivity.java
@@ -2,13 +2,46 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private ImageButton btn_back;
+    private Button btn_register_save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Intent intent = getIntent();
+
+        btn_register_save = findViewById(R.id.btn_register_save);
+        btn_back = findViewById(R.id.btn_back);
+
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                finish();
+            }
+        });
+
+        btn_register_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // setResult 및 Intent 수정 필요
+                setResult(0);
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        });
+
     }
 }
