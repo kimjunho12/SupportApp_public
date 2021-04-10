e6b126d6f0ca1e13127bc0e769c0cf384c727244
SeoHyunWoo <tjgusdn07@naver.com>
2021-04-10 14:16:50 +0900

    bottom_account_layout 구현

--- app/src/main/java/com/example/myapplication/bottom_account_fragment.java
+++ app/src/main/java/com/example/myapplication/bottom_account_fragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,11 +14,13 @@ import androidx.fragment.app.Fragment;
 
 public class bottom_account_fragment extends Fragment {
     private View view;
+    private Button input_save;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.bottom_account_fragment, container, false);
+
         return view;
     }
 }
