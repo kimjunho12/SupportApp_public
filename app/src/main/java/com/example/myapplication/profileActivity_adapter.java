b65f153438c747e19189448074c9c807a34f6862
SeoHyunWoo <tjgusdn07@naver.com>
2021-04-11 20:55:04 +0900

    profile 액티비티 구현 끝

--- app/src/main/java/com/example/myapplication/profileActivity_adapter.java
+++ app/src/main/java/com/example/myapplication/profileActivity_adapter.java
@@ -14,44 +14,38 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class bottom_home_adapter extends RecyclerView.Adapter<bottom_home_adapter.CustomViewHolder> {
+public class profileActivity_adapter extends RecyclerView.Adapter<profileActivity_adapter.CustomViewHolder> {
     private Context context;
     private List<bottom_home_data> list = new ArrayList<>();
 
-
-    public bottom_home_adapter(Context context, ArrayList<bottom_home_data> list) {
-        this.context = context;
+    public profileActivity_adapter(ArrayList<bottom_home_data> list) {
+        //this.context = context;
         this.list = list;
     }
 
     @NonNull
     @Override
-    public bottom_home_adapter.CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public profileActivity_adapter.CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.bottom_home_recyclerview, parent, false);
-        bottom_home_adapter.CustomViewHolder holder = new CustomViewHolder(view);
+        profileActivity_adapter.CustomViewHolder holder = new CustomViewHolder(view);
         return holder;
     }
-
-    @Override
-    public void onBindViewHolder(@NonNull CustomViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull profileActivity_adapter.CustomViewHolder holder, int position) {
         holder.news_image1.setImageResource(list.get(position).getBanner());
         holder.news_image2.setImageResource(list.get(position).getBanner1());
         holder.itemView.setTag(position);
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-              Intent intent = new Intent(view.getContext(), newsActivity.class);
+                Intent intent = new Intent(view.getContext(), newsActivity.class);
                 view.getContext().startActivity(intent);
             }
         });
     }
-
     @Override
     public int getItemCount() {
-            return list.size();
-            }
-
-
+        return list.size();
+    }
     class CustomViewHolder extends RecyclerView.ViewHolder {
         protected ImageView news_image1;
         protected ImageView news_image2;
@@ -64,9 +58,3 @@ public class bottom_home_adapter extends RecyclerView.Adapter<bottom_home_adapte
         }
     }
 }
-
-
-
-
-
-
---
+++ app/src/main/java/com/example/myapplication/support_popupActivity.java
@@ -0,0 +1,61 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class support_popupActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //타이틀바 없애기
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_supportpopup);
+
+
+        //UI 객체생성
+        //textView = (TextView)findViewById(R.id.txtText);
+
+        //데이터 가져오기
+        /*Intent intent = getIntent();
+        String data = intent.getStringExtra("data");
+        textView.setText(data);*/
+    }
+
+    //확인 버튼 클릭
+    public void profile_support_close(View view){
+        //데이터 전달하기
+        /*Intent intent = new Intent();
+        intent.putExtra("result", "Close Popup");
+        setResult(RESULT_OK, intent);*/
+
+        //액티비티(팝업) 닫기
+        finish();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //바깥레이어 클릭시 안닫히게
+        if(event.getAction()==MotionEvent.ACTION_OUTSIDE){
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        //안드로이드 백버튼 막기
+        return;
+    }
+}
