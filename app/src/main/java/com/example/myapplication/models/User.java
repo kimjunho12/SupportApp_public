4eac07df8b09b09ba726894e9d873188f0c78c21
98kimjh <98kimjh@gmail.com>
2021-08-29 17:16:59 +0900

    회원가입 시 생일 안넘어가던 오류 해결

--- app/src/main/java/com/example/myapplication/models/User.java
+++ app/src/main/java/com/example/myapplication/models/User.java
@@ -24,6 +24,7 @@ public class User {
         this.pw = pw;
         this.name = name;
         this.phone = phone;
+        this.birth = birth;
         this.photoURL = "null";
         this.is_target = is_target;
     }
@@ -34,6 +35,7 @@ public class User {
         this.pw = pw;
         this.name = name;
         this.phone = phone;
+        this.birth = birth;
         this.photoURL = photoURL;
         this.is_target = is_target;
     }
