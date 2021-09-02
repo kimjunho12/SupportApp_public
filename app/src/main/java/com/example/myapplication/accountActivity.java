8ea22662de59053094696b66ed58e47be9524696
98kimjh <98kimjh@gmail.com>
2021-09-02 17:25:08 +0900

    계정정보 변경 후원대상 구분 및 기능 추가

--- app/src/main/java/com/example/myapplication/accountActivity.java
+++ app/src/main/java/com/example/myapplication/accountActivity.java
@@ -1,13 +1,7 @@
 package com.example.myapplication;
 
-import android.app.Activity;
-import android.os.Bundle;
-import androidx.annotation.NonNull;
-import static android.app.Activity.RESULT_OK;
-import static android.content.ContentValues.TAG;
-import static android.content.Intent.getIntent;
-
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
@@ -15,34 +9,19 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 import androidx.loader.content.CursorLoader;
 
 import com.bumptech.glide.Glide;
-import com.example.myapplication.models.Post;
-import com.example.myapplication.models.Target;
-import com.example.myapplication.models.User;
-import com.example.myapplication.models.bottom_favorite_profile_model;
-import com.example.myapplication.models.bottom_home_data;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.common.collect.Interner;
-import com.google.common.util.concurrent.Uninterruptibles;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -51,18 +30,16 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
-import com.kakao.sdk.story.model.BirthdayType;
 
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class accountActivity extends Activity {
 
     private static final int GET_GALLARY = 100;
+    private static final String TAG = "accountPage";
     private FirebaseAuth mAuth;
     private FirebaseStorage storage;
     private FirebaseDatabase database;
@@ -80,70 +57,15 @@ public class accountActivity extends Activity {
     private Button account_image_change_profile;
     private Button account_details_save;
     private String uri_imagepath;
+    private int is_target;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
 
-        mAuth = FirebaseAuth.getInstance();
-        storage = FirebaseStorage.getInstance();
+        init();
 
-        input_name = findViewById(R.id.input_name);
-        input_debut_date = findViewById(R.id.input_debut_date);
-        input_phone_no = findViewById(R.id.input_phone_no);
-        input_pr = findViewById(R.id.input_pr);
-        input_SNS = findViewById(R.id.input_SNS);
-        input_pr = findViewById(R.id.input_pr);
-        input_birth_date = findViewById(R.id.input_birth_date);
-        input_sosock = findViewById(R.id.input_sosock);
-        input_image = findViewById(R.id.account_image);
-        account_image_change_profile = (Button) findViewById(R.id.account_image_change_profile);
-        account_details_save = (Button) findViewById(R.id.account_details_save);
-
-        Uid = mAuth.getUid();
-        Log.d(TAG, "Uid : " + Uid);
-
-        database = FirebaseDatabase.getInstance();
-        databaseReference = database.getReference("Users");
-        databaseReference.child(Uid).addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
-                String name = snapshot.child("name").getValue().toString();
-                String phone_no = snapshot.child("phone").getValue().toString();
-                String birth_date = snapshot.child("birth").getValue().toString();
-                //이미지 받아오기
-                String icon = snapshot.child("photoURL").getValue().toString();
-                uri_imagepath = icon;
-                File file = new File(icon);
-                String strFileName = file.getName();
-                FirebaseStorage storage = FirebaseStorage.getInstance("gs://supportapp-f34a1.appspot.com");
-                StorageReference storageReference = storage.getReference();
-                storageReference.child("images/" + strFileName).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                    @Override
-                    public void onSuccess(Uri uri) {
-                        Log.d(TAG, "img uri : " + uri);
-                        Glide.with(accountActivity.this).load(uri).into(input_image);
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull @NotNull Exception e) {
-                    }
-                });
-                //String sosock = snapshot.child("sosock").getValue().toString();
-                //String debut_date = snapshot.child("debut_date").getValue().toString();
-                //String SNS = snapshot.child("SNS").getValue().toString();
-                //String pr = snapshot.child("pr").getValue().toString();
-                //Glide.with(accountActivity.this).load(icon).into(input_image);
-                input_name.setText(name);
-                input_phone_no.setText(phone_no);
-                input_birth_date.setText(birth_date);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Log.e("accountActivity error", String.valueOf(error.toException())); //에러 시 출력
-            }
-        });
+        loadInfo();
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
@@ -160,24 +82,10 @@ public class accountActivity extends Activity {
         account_details_save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                        String name = input_name.getText().toString();
-                        String phone = input_phone_no.getText().toString();
-                        String birth = input_birth_date.getText().toString();
-                        upload(imagePath);
-                        database = FirebaseDatabase.getInstance();
-                        databaseReference = database.getReference("Users");
-                        databaseReference.child(Uid).child("name").setValue(name);
-                        databaseReference.child(Uid).child("phone").setValue(phone);
-                        databaseReference.child(Uid).child("birth").setValue(birth);
-                        if(imagePath == null) {
-                            databaseReference.child(Uid).child("photoURL").setValue(uri_imagepath);
-                        }
-                        else
-                            databaseReference.child(Uid).child("photoURL").setValue(imagePath);
-                        finish();
-                    }
-                });
+                updateInfo();
             }
+        });
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -202,6 +110,9 @@ public class accountActivity extends Activity {
     }
 
     private void upload(String uri) {
+        if (uri == null) {
+            return;
+        }
         StorageReference storageRef = storage.getReferenceFromUrl("gs://supportapp-f34a1.appspot.com");
         Uri file = Uri.fromFile(new File(uri));
         StorageReference riversRef = storageRef.child("images/" + file.getLastPathSegment());
@@ -219,5 +130,149 @@ public class accountActivity extends Activity {
                 Task<Uri> downloadUrl = taskSnapshot.getStorage().getDownloadUrl();
             }
         });
+    }
+
+    private void init() {
+        mAuth = FirebaseAuth.getInstance();
+        storage = FirebaseStorage.getInstance();
+
+        input_name = findViewById(R.id.input_name);
+        input_debut_date = findViewById(R.id.input_debut_date);
+        input_phone_no = findViewById(R.id.input_phone_no);
+        input_pr = findViewById(R.id.input_pr);
+        input_SNS = findViewById(R.id.input_SNS);
+        input_pr = findViewById(R.id.input_pr);
+        input_birth_date = findViewById(R.id.input_birth_date);
+        input_sosock = findViewById(R.id.input_sosock);
+        input_image = findViewById(R.id.account_image);
+        account_image_change_profile = (Button) findViewById(R.id.account_image_change_profile);
+        account_details_save = (Button) findViewById(R.id.account_details_save);
+
+        Uid = mAuth.getUid();
+        Log.d(TAG, "Uid : " + Uid);
+    }
+
+    private void loadInfo() {
+        database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference("Users");
+        databaseReference.child(Uid).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                String name = snapshot.child("name").getValue().toString();
+                String phone_no = snapshot.child("phone").getValue().toString();
+                String birth_date = snapshot.child("birth").getValue().toString();
+                //이미지 받아오기
+                String icon = snapshot.child("photoURL").getValue().toString();
+                is_target = snapshot.child("is_target").getValue(Integer.class);
+                // 후원대상일 경우 추가 정보 받아오기
+                if (is_target == 1) {
+                    loadTargetInfo();
+                }
+                uri_imagepath = icon;
+                File file = new File(icon);
+                String strFileName = file.getName();
+                FirebaseStorage storage = FirebaseStorage.getInstance("gs://supportapp-f34a1.appspot.com");
+                StorageReference storageReference = storage.getReference();
+                storageReference.child("images/" + strFileName).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        Log.d(TAG, "img uri : " + uri);
+                        Glide.with(accountActivity.this).load(uri).into(input_image);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull @NotNull Exception e) {
+                    }
+                });
+                //Glide.with(accountActivity.this).load(icon).into(input_image);
+                input_name.setText(name);
+                input_phone_no.setText(phone_no);
+                input_birth_date.setText(birth_date);
+//                // 후원대상이 아닐경우 나머지 정보 감추기
+//                input_sosock.setVisibility(View.GONE);
+//                input_debut_date.setVisibility(View.GONE);
+//                input_SNS.setVisibility(View.GONE);
+//                input_pr.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e("accountActivity error", String.valueOf(error.toException())); //에러 시 출력
+            }
+        });
+    }
+
+    private void loadTargetInfo() {
+        DatabaseReference targetDB = FirebaseDatabase.getInstance().getReference("target");
+        targetDB.child(Uid).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Log.d(TAG, "onDataChange: target's snapshot = " + snapshot);
+                String sosock = snapshot.child("team").getValue().toString();
+                String debut_date = snapshot.child("debut").getValue().toString();
+                String SNS = snapshot.child("sns").getValue().toString();
+                String pr = snapshot.child("intro").getValue().toString();
+
+                input_sosock.setText(sosock);
+                input_debut_date.setText(debut_date);
+                input_SNS.setText(SNS);
+                input_pr.setText(pr);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e("accountActivity error", String.valueOf(error.toException())); //에러 시 출력
+            }
+        });
+    }
+
+    private void updateInfo() {
+        String name = input_name.getText().toString();
+        String phone = input_phone_no.getText().toString();
+        String birth = input_birth_date.getText().toString();
+        upload(imagePath);
+        database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference("Users");
+        databaseReference.child(Uid).child("name").setValue(name);
+        databaseReference.child(Uid).child("phone").setValue(phone);
+        databaseReference.child(Uid).child("birth").setValue(birth);
+        if (imagePath == null) {
+            databaseReference.child(Uid).child("photoURL").setValue(uri_imagepath);
+        } else
+            databaseReference.child(Uid).child("photoURL").setValue(imagePath);
+
+        if (is_target == 1) {
+            updateTargetInfo();
+        }
+        Log.d(TAG, "updateInfo: is finished");
+        finish();
+    }
+
+    private void updateTargetInfo() {
+        DatabaseReference targetDB = FirebaseDatabase.getInstance().getReference("target");
+
+        String name = input_name.getText().toString();
+        String phone = input_phone_no.getText().toString();
+        String birth = input_birth_date.getText().toString();
+        String sosock = input_sosock.getText().toString();
+        String debut_date = input_debut_date.getText().toString();
+        String SNS = input_SNS.getText().toString();
+        String pr = input_pr.getText().toString();
+
+        targetDB.child(Uid).child("name").setValue(name);
+        targetDB.child(Uid).child("phone").setValue(phone);
+        targetDB.child(Uid).child("birth").setValue(birth);
+        targetDB.child(Uid).child("team").setValue(sosock);
+        targetDB.child(Uid).child("subject").child("sCategory").setValue(sosock);
+        targetDB.child(Uid).child("debut").setValue(debut_date);
+        targetDB.child(Uid).child("sns").setValue(SNS);
+        targetDB.child(Uid).child("intro").setValue(pr);
+
+        if (imagePath == null) {
+            targetDB.child(Uid).child("icon").setValue(uri_imagepath);
+        } else
+            targetDB.child(Uid).child("icon").setValue(imagePath);
+
+        Log.d(TAG, "updateTargetInfo: is finished");
     }
 }
