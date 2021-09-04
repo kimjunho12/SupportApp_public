32596f16f2188c66965e928de3e05674ad4c3534
진범진 <jinbeomjin@google.com>
2021-09-05 03:12:41 +0900

    9/5 // 뷰페이저 인디케이터, 및 무한스크롤완성

--- app/src/main/java/com/example/myapplication/bottom_home_viewpager_adapter.java
+++ app/src/main/java/com/example/myapplication/bottom_home_viewpager_adapter.java
@@ -4,14 +4,18 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import java.util.ArrayList;
 
-public class bottom_home_viewpager_adapter extends FragmentPagerAdapter {
+public class bottom_home_viewpager_adapter extends FragmentStateAdapter {
     private ArrayList<Fragment> items = new ArrayList<Fragment>();
+    public int mCount;
 
-    public bottom_home_viewpager_adapter(@NonNull FragmentManager fm) {
+    public bottom_home_viewpager_adapter(@NonNull bottom_home_fragment fm, int count) {
         super(fm);
+        mCount = count;
     }
 
     public void addItem(Fragment item) {
@@ -19,11 +23,19 @@ public class bottom_home_viewpager_adapter extends FragmentPagerAdapter {
     }
     @NonNull
     @Override
-    public Fragment getItem(int position) {
-        return items.get(position);
+    public Fragment createFragment(int position) {
+        int index = getRealPosition(position);
+
+        if(index==0) return new viewpager_FirstFragment();
+        else if(index==1) return new viewpager_SecondFragment();
+        else return new viewpager_ThirdFragment();
     }
-    @Override
-    public int getCount() {
-        return items.size();
+
+    public int getItemCount() {
+        return 2000;
     }
+        public int getRealPosition (int position){
+            return position % mCount;
+        }
+
 }
